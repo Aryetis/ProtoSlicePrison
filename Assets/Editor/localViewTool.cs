b37a327d2c1057226a2752b09732f4aa04a0c2e9
HijackedBrain <aramirg@hotmail.fr>
2017-11-03 11:31:22 +0100

    updated localViewTool

--- Assets/Editor/localViewTool.cs
+++ Assets/Editor/localViewTool.cs
@@ -1,27 +1,48 @@
-﻿using UnityEngine;
+﻿/*
+ * Author : Aryetis
+ * Copyright : CC BY-NC-SA 3.0
+ */
+
+using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
 
 public class localViewTool : EditorWindow 
 {
-	static List<GameObject> hiddenObjects = new List<GameObject>();
+    [SerializeField] static List<GameObject> hiddenObjects = new List<GameObject>();
 
-    // TODO : Add take care of hiding/restoring/showing only children of selected GameObject too if any
+    /* TODO : Add take care of hiding/restoring/showing only children of selected GameObject too if any
+     *        check what happens if we close editor with some gameobjects "turned off" 
+     * 
+     */
 
 	[MenuItem ("Tools/localView/HideSelection %&h")]
 	static void HideSelection ()
 	{
-		foreach(GameObject go in UnityEditor.Selection.gameObjects)
-			if (go.GetComponent<Renderer>() != null)
-			{
-				go.GetComponent<Renderer>().enabled = false ;
-				hiddenObjects.Add(go);
-			}
+        foreach (GameObject go in UnityEditor.Selection.gameObjects)
+            setSelectionState(go, false);
 	}
+
+    static void setSelectionState(GameObject go, bool status)
+    {
+        // Deactivate current renderer
+        if (go.GetComponent<Renderer>() != null)
+        {
+            go.GetComponent<Renderer>().enabled = status;
+            hiddenObjects.Add(go);
+        }
+
+        // Explore children
+        int childrenCount = go.transform.childCount;
+        foreach (Transform goct in go.transform)
+            setSelectionState(goct.gameObject, status);
+    }
+
 
 	[MenuItem ("Tools/localView/ShowOnlySelection %&s")]
 	static void ShowOnlySelection ()
 	{
+        // Hide all objects
 		foreach(GameObject go in FindObjectsOfType(typeof(GameObject)) as GameObject[])
 			if (go.GetComponent<Renderer>() != null)
 			{
@@ -29,8 +50,9 @@ public class localViewTool : EditorWindow
 				hiddenObjects.Add(go);
 			}
 
+        // Show selection
 		foreach(GameObject go in UnityEditor.Selection.gameObjects)
-			go.GetComponent<Renderer>().enabled = true ;
+            setSelectionState(go, true);
 	}
 
 	[MenuItem ("Tools/localView/RestoreHiddenObjects %&r")]
@@ -43,7 +65,7 @@ public class localViewTool : EditorWindow
 	[MenuItem ("Tools/localView/Options")]
 	static void Options ()
 	{
-		//TODO hide only ? hide + non interactable ? + AssignHotkeys
+		//TODO AssignHotkeys
 		Debug.Log ("Doing Something...");
 		localViewTool window = (localViewTool)EditorWindow.GetWindow (typeof (localViewTool));
 		window.Show();
