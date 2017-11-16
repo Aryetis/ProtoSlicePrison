a8b98da5686016511151828523b3cd3514660e26
HijackedBrain <aramirg@hotmail.fr>
2017-11-16 14:28:53 +0100

    removing Debug.Log

--- Assets/Internal Assets/Scripts/EnnemyBehavior.cs
+++ Assets/Internal Assets/Scripts/EnnemyBehavior.cs
@@ -203,10 +203,8 @@ public class EnnemyBehavior : MonoBehaviour
 
 	void walkingAction()
 	{
-        Debug.Log("target : " + target.name);
         if (!Physics.Linecast(transform.position, target.transform.position, ~(1 << 8 | 1 << 9))) // ( ~ <=> inverse bits) ignore layer 8 and 9
         {
-            Debug.Log("following");
             // check if there is collision with, anyLayer - Layer 8 "Ignore LineCast" and 9
             transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.transform.position.x, 0, target.transform.position.z), speed *  Time.deltaTime);
         }
